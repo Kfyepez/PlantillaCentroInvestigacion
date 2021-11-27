d98b913e9a1be5a5e6fe42923a29414f74d72ee1
gabicuadros <icuadros@espol.edu.ec>
2021-11-27 00:42:08 -0500

    contactenos

---
+++ web/sites/webk.dd/files/php/twig/61a00f1b0a87d_links--media-library-menu_VvYZzHg4QQ-WxMoDGAGKHCjD1/GjUdo6w-YSfqbns8XC9oQu7K7SsFOPgWxozxk5WYywQ.php
@@ -0,0 +1,96 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/seven/templates/classy/media-library/links--media-library-menu.html.twig */
+class __TwigTemplate_450be3b02f7798fd60e2e064c07a8b32b77b8f285d0c9b6eea952d9900a2cd34 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "links.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 36
+        $context["attributes"] = twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "media-library-menu"], "method", false, false, true, 36);
+        // line 1
+        $this->parent = $this->loadTemplate("links.html.twig", "core/themes/seven/templates/classy/media-library/links--media-library-menu.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/seven/templates/classy/media-library/links--media-library-menu.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 1,  43 => 36,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/seven/templates/classy/media-library/links--media-library-menu.html.twig", "C:\\Users\\Kevin\\Desktop\\Proyecto ING\\PlantillaCentroInvestigacion\\web\\core\\themes\\seven\\templates\\classy\\media-library\\links--media-library-menu.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 36);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
